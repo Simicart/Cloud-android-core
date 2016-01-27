b94e63be2b3a9f82ec6c43cc2c02efceaee8d1c3
Martial <crabby@gmail.com>
2016-01-27 10:46:32 +0700

    Fix bug not display button and layout in product detail was cached. Delete unused code

--- app/src/main/java/com/simicart/plugins/braintree/Settings.java
+++ app/src/main/java/com/simicart/plugins/braintree/Settings.java
@@ -6,54 +6,10 @@ import android.preference.PreferenceManager;
 
 public class Settings {
 
-    protected static final String ENVIRONMENT = "environment";
-
-    private static final String SANDBOX_BASE_SERVER_URL = "https://braintree-sample-merchant.herokuapp.com";
-    private static final String PRODUCTION_BASE_SERVER_URL = "https://executive-sample-merchant.herokuapp.com";
-
     private static final String SANDBOX_TOKENIZATION_KEY = "sandbox_tmxhyf7d_3yzdm69mrvcpy3z3";
-    private static final String PRODUCTION_TOKENIZATION_KEY = "production_t2wns2y2_3yzdm69mrvcpy3z3";
 
     public static SharedPreferences getPreferences(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
-    }
-
-    public static int getEnvironment(Context context) {
-        return getPreferences(context).getInt(ENVIRONMENT, 0);
-    }
-
-    public static void setEnvironment(Context context, int environment) {
-        getPreferences(context)
-                .edit()
-                .putInt(ENVIRONMENT, environment)
-                .apply();
-    }
-
-    public static String getSandboxUrl() {
-        return SANDBOX_BASE_SERVER_URL;
-    }
-
-    public static String getEnvironmentUrl(Context context) {
-        int environment = getEnvironment(context);
-        if (environment == 0) {
-            return SANDBOX_BASE_SERVER_URL;
-        } else if (environment == 1) {
-            return PRODUCTION_BASE_SERVER_URL;
-        } else {
-            return "";
-        }
-    }
-
-    public static String getCustomerId(Context context) {
-        return getPreferences(context).getString("customer", null);
-    }
-
-    public static String getThreeDSecureMerchantAccountId(Context context) {
-        if (isThreeDSecureEnabled(context) && getEnvironment(context) == 1) {
-            return "test_AIB";
-        } else {
-            return null;
-        }
     }
 
     public static boolean useTokenizationKey(Context context) {
@@ -61,34 +17,6 @@ public class Settings {
     }
 
     public static String getEnvironmentTokenizationKey(Context context) {
-//        int environment = getEnvironment(context);
-//        if (environment == 0) {
-//            return SANDBOX_TOKENIZATION_KEY;
-//        } else if (environment == 1) {
-//            return PRODUCTION_TOKENIZATION_KEY;
-//        } else {
-//            return "";
-//        }
         return SANDBOX_TOKENIZATION_KEY;
-    }
-
-    public static boolean isAndroidPayShippingAddressRequired(Context context) {
-        return getPreferences(context).getBoolean("android_pay_require_shipping_address", false);
-    }
-
-    public static boolean isAndroidPayPhoneNumberRequired(Context context) {
-        return getPreferences(context).getBoolean("android_pay_require_phone_number", false);
-    }
-
-    public static boolean isPayPalAddressScopeRequested(Context context) {
-        return getPreferences(context).getBoolean("paypal_request_address_scope", false);
-    }
-
-    public static boolean isThreeDSecureEnabled(Context context) {
-        return getPreferences(context).getBoolean("enable_three_d_secure", false);
-    }
-
-    public static boolean isThreeDSecureRequired(Context context) {
-        return getPreferences(context).getBoolean("require_three_d_secure", true);
     }
 }
