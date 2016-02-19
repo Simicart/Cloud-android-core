677be2867b5471c8f9aa156d2704ae9703fba985
dongln <dongln3011@gmail.com>
2016-02-19 14:51:05 +0700

    Change function check name, last name, first name

--- app/src/main/java/com/simicart/plugins/facebooklogin/AutoSignInFacebook.java
+++ app/src/main/java/com/simicart/plugins/facebooklogin/AutoSignInFacebook.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
 import com.simicart.core.base.delegate.ModelDelegate;
 import com.simicart.core.base.manager.SimiManager;
 import com.simicart.core.base.model.collection.SimiCollection;
@@ -26,193 +27,192 @@ import java.util.ArrayList;
 
 public class AutoSignInFacebook {
 
-	public AutoSignInFacebook() {
-		Context context = SimiManager.getIntance().getCurrentContext();
+    public AutoSignInFacebook() {
+        Context context = SimiManager.getIntance().getCurrentContext();
 
-		// register event: autosigin facebook
-		IntentFilter autosigin_filter = new IntentFilter("com.simicart.autoSignIn");
-		BroadcastReceiver autosigin_receiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				autoSignIn();
-			}
-		};
-		LocalBroadcastManager.getInstance(context).registerReceiver(autosigin_receiver, autosigin_filter);
-	}
+        // register event: autosigin facebook
+        IntentFilter autosigin_filter = new IntentFilter("com.simicart.autoSignIn");
+        BroadcastReceiver autosigin_receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                autoSignIn();
+            }
+        };
+        LocalBroadcastManager.getInstance(context).registerReceiver(autosigin_receiver, autosigin_filter);
+    }
 
-	private void autoSignIn() {
-		final String email = DataLocal.getEmail();
-		final String first_name = DataLocal.getCustomer().getFirstName();
-		final String last_name = DataLocal.getCustomer().getLastName();
-		final FacebookModel model = new FacebookModel();
-		model.setDelegate(new ModelDelegate() {
+    private void autoSignIn() {
+        final String email = DataLocal.getEmail();
+        final String first_name = DataLocal.getCustomer().getFirstName();
+        final String last_name = DataLocal.getCustomer().getLastName();
+        final FacebookModel model = new FacebookModel();
+        model.setDelegate(new ModelDelegate() {
 
-			@Override
-			public void onFail(SimiError error) {
+            @Override
+            public void onFail(SimiError error) {
 
-			}
+            }
 
-			@Override
-			public void onSuccess(SimiCollection collection) {
-				if(collection != null && collection.getCollection().size() > 0) {
-					ProfileEntity entity = (ProfileEntity) collection.getCollection().get(0);
-					DataLocal.saveTypeSignIn("facebook");
-					DataLocal.saveSignInState(true);
+            @Override
+            public void onSuccess(SimiCollection collection) {
+                if (collection != null && collection.getCollection().size() > 0) {
+                    ProfileEntity entity = (ProfileEntity) collection.getCollection().get(0);
+                    DataLocal.saveTypeSignIn("facebook");
+                    DataLocal.saveSignInState(true);
 
-					String email = "";
-					String firstname = "";
-					String lastname = "";
-					String name = "";
-					String customerID = "";
+                    String email = "";
+                    String firstname = "";
+                    String lastname = "";
+                    String name = "";
+                    String customerID = "";
 
-					if (entity.getEmail() != null)
-						email = entity.getEmail();
-					if (entity.getFirstName() != null && entity.getLastName() != null)
-						name = entity.getFirstName() + " " + entity.getLastName();
-					if (entity.getID() != null)
-						customerID = entity.getID();
-					if (entity.getFirstName() != null)
-						firstname = entity.getFirstName();
-					if (entity.getLastName() != null)
-						lastname = entity.getLastName();
+                    if (entity.getEmail() != null)
+                        email = entity.getEmail();
+                    if (entity.getID() != null)
+                        customerID = entity.getID();
+                    if (entity.getFirstName() != null)
+                        firstname = entity.getFirstName();
+                    if (entity.getLastName() != null)
+                        lastname = entity.getLastName();
 
-					if (null != name) {
-						Log.e("FacebookLogin", name + " " + email);
-						DataLocal.saveData(name, email);
-						DataLocal.saveCustomerID(customerID);
-						DataLocal.saveCustomer(firstname, lastname, email, name, customerID);
-					}
-					SimiManager.getIntance().onUpdateItemSignIn();
-					requestGetAllQuote();
-				}
-			}
-		});
-		model.addDataExtendURL("facebook-login");
-		model.addDataBody("email", "" + email + "");
-		model.addDataBody("first_name", "" + first_name + "");
-		model.addDataBody("last_name", "" + last_name + "");
-		model.request();
-	}
+                    name = entity.getFirstName() + " " + entity.getLastName();
 
-	private void requestGetAllQuote() {
-		GetAllQuoteModel quoteModel = new GetAllQuoteModel();
-		quoteModel.setDelegate(new ModelDelegate() {
-			@Override
-			public void onFail(SimiError error) {
-				if(error != null) {
-					SimiManager.getIntance().showNotify(null, error.getMessage(), "OK");
-				}
-			}
+                    Log.e("FacebookLogin", name + " " + email + " " + customerID);
+                    DataLocal.saveData(name, email);
+                    DataLocal.saveCustomerID(customerID);
+                    DataLocal.saveCustomer(firstname, lastname, email, name, customerID);
 
-			@Override
-			public void onSuccess(SimiCollection collection) {
-				if (collection != null) {
-					ArrayList<SimiEntity> entity = collection.getCollection();
-					if (entity != null && entity.size() > 0) {
-						ArrayList<QuoteEntity> listQuote = new ArrayList<QuoteEntity>();
-						for (SimiEntity simiEntity : entity) {
-							QuoteEntity quoteEntity = (QuoteEntity) simiEntity;
-							listQuote.add(quoteEntity);
-						}
+                    SimiManager.getIntance().onUpdateItemSignIn();
+                    requestGetAllQuote();
+                }
+            }
+        });
+        model.addDataExtendURL("facebook-login");
+        model.addDataBody("email", "" + email + "");
+        model.addDataBody("first_name", "" + first_name + "");
+        model.addDataBody("last_name", "" + last_name + "");
+        model.request();
+    }
 
-						if (listQuote.size() > 0) {
-							Config.getInstance().setQuoteCustomerSignIn(listQuote.get(0).getID());
-							SimiManager.getIntance().onUpdateCartQty(String.valueOf(listQuote.get(0).getQty()));
-						}
+    private void requestGetAllQuote() {
+        GetAllQuoteModel quoteModel = new GetAllQuoteModel();
+        quoteModel.setDelegate(new ModelDelegate() {
+            @Override
+            public void onFail(SimiError error) {
+                if (error != null) {
+                    SimiManager.getIntance().showNotify(null, error.getMessage(), "OK");
+                }
+            }
 
-						if (Config.getInstance().getQuoteCustomerSignIn().equals("")) {
-							if (!DataLocal.getQuoteCustomerNotSigin().equals("")) {
-								requestUpdateCustomerToQuote();
-							}
-						} else {
-							if (!DataLocal.getQuoteCustomerNotSigin().equals("")) {
-								requestMergeQuote();
-							}
-						}
-					}
-				}
-			}
-		});
+            @Override
+            public void onSuccess(SimiCollection collection) {
+                if (collection != null) {
+                    ArrayList<SimiEntity> entity = collection.getCollection();
+                    if (entity != null && entity.size() > 0) {
+                        ArrayList<QuoteEntity> listQuote = new ArrayList<QuoteEntity>();
+                        for (SimiEntity simiEntity : entity) {
+                            QuoteEntity quoteEntity = (QuoteEntity) simiEntity;
+                            listQuote.add(quoteEntity);
+                        }
 
-		quoteModel.addDataParameter("filter[customer|customer_id]", DataLocal.getCustomerID());
-		quoteModel.addDataParameter("filter[orig_order_id]", "0");
-		quoteModel.request();
-	}
+                        if (listQuote.size() > 0) {
+                            Config.getInstance().setQuoteCustomerSignIn(listQuote.get(0).getID());
+                            SimiManager.getIntance().onUpdateCartQty(String.valueOf(listQuote.get(0).getQty()));
+                        }
 
-	private void requestMergeQuote() {
-		MergeQuoteModel mergeQuoteModel = new MergeQuoteModel();
-		mergeQuoteModel.setDelegate(new ModelDelegate() {
-			@Override
-			public void onFail(SimiError error) {
-				if(error != null) {
-					SimiManager.getIntance().showNotify(null, error.getMessage(), "OK");
-				}
-			}
+                        if (Config.getInstance().getQuoteCustomerSignIn().equals("")) {
+                            if (!DataLocal.getQuoteCustomerNotSigin().equals("")) {
+                                requestUpdateCustomerToQuote();
+                            }
+                        } else {
+                            if (!DataLocal.getQuoteCustomerNotSigin().equals("")) {
+                                requestMergeQuote();
+                            }
+                        }
+                    }
+                }
+            }
+        });
 
-			@Override
-			public void onSuccess(SimiCollection collection) {
-				if (collection != null) {
-					if (collection.getCollection().size() > 0) {
-						QuoteEntity quoteEntity = (QuoteEntity) collection.getCollection().get(0);
-						SimiManager.getIntance().onUpdateCartQty(String.valueOf(quoteEntity.getQty()));
-						Config.getInstance().setQuoteCustomerSignIn(quoteEntity.getID());
-						DataLocal.saveQuoteCustomerNotSignIn("");
-					}
-				}
-			}
-		});
+        quoteModel.addDataParameter("filter[customer|customer_id]", DataLocal.getCustomerID());
+        quoteModel.addDataParameter("filter[orig_order_id]", "0");
+        quoteModel.request();
+    }
 
-		mergeQuoteModel.addDataExtendURL("merge");
-		mergeQuoteModel.addDataBody("source_quoteId", DataLocal.getQuoteCustomerNotSigin());
-		mergeQuoteModel.addDataBody("des_quoteId", Config.getInstance().getQuoteCustomerSignIn());
-		mergeQuoteModel.request();
-	}
+    private void requestMergeQuote() {
+        MergeQuoteModel mergeQuoteModel = new MergeQuoteModel();
+        mergeQuoteModel.setDelegate(new ModelDelegate() {
+            @Override
+            public void onFail(SimiError error) {
+                if (error != null) {
+                    SimiManager.getIntance().showNotify(null, error.getMessage(), "OK");
+                }
+            }
 
-	private void requestUpdateCustomerToQuote() {
-		UpdateCustomerToQuoteModel updateCustomerToQuoteModel = new UpdateCustomerToQuoteModel();
-		updateCustomerToQuoteModel.setDelegate(new ModelDelegate() {
-			@Override
-			public void onFail(SimiError error) {
-				if (error != null) {
-					SimiManager.getIntance().showNotify(null, error.getMessage(), "OK");
-				}
-			}
+            @Override
+            public void onSuccess(SimiCollection collection) {
+                if (collection != null) {
+                    if (collection.getCollection().size() > 0) {
+                        QuoteEntity quoteEntity = (QuoteEntity) collection.getCollection().get(0);
+                        SimiManager.getIntance().onUpdateCartQty(String.valueOf(quoteEntity.getQty()));
+                        Config.getInstance().setQuoteCustomerSignIn(quoteEntity.getID());
+                        DataLocal.saveQuoteCustomerNotSignIn("");
+                    }
+                }
+            }
+        });
 
-			@Override
-			public void onSuccess(SimiCollection collection) {
-				if (collection != null) {
-					if (collection.getCollection().size() > 0) {
-						QuoteEntity quoteEntity = (QuoteEntity) collection.getCollection().get(0);
-						Config.getInstance().setQuoteCustomerSignIn(quoteEntity.getID());
-						DataLocal.saveQuoteCustomerNotSignIn("");
-					}
-				}
-			}
-		});
+        mergeQuoteModel.addDataExtendURL("merge");
+        mergeQuoteModel.addDataBody("source_quoteId", DataLocal.getQuoteCustomerNotSigin());
+        mergeQuoteModel.addDataBody("des_quoteId", Config.getInstance().getQuoteCustomerSignIn());
+        mergeQuoteModel.request();
+    }
 
-		if (!Config.getInstance().getQuoteCustomerSignIn().equals("")) {
-			updateCustomerToQuoteModel.addDataExtendURL(Config.getInstance().getQuoteCustomerSignIn());
-		}
+    private void requestUpdateCustomerToQuote() {
+        UpdateCustomerToQuoteModel updateCustomerToQuoteModel = new UpdateCustomerToQuoteModel();
+        updateCustomerToQuoteModel.setDelegate(new ModelDelegate() {
+            @Override
+            public void onFail(SimiError error) {
+                if (error != null) {
+                    SimiManager.getIntance().showNotify(null, error.getMessage(), "OK");
+                }
+            }
 
-		if (!DataLocal.getQuoteCustomerNotSigin().equals("")) {
-			updateCustomerToQuoteModel.addDataExtendURL(DataLocal.getQuoteCustomerNotSigin());
-		}
+            @Override
+            public void onSuccess(SimiCollection collection) {
+                if (collection != null) {
+                    if (collection.getCollection().size() > 0) {
+                        QuoteEntity quoteEntity = (QuoteEntity) collection.getCollection().get(0);
+                        Config.getInstance().setQuoteCustomerSignIn(quoteEntity.getID());
+                        DataLocal.saveQuoteCustomerNotSignIn("");
+                    }
+                }
+            }
+        });
 
-		JSONObject cutomer = null;
-		try {
-			cutomer = new JSONObject();
-			cutomer.put("customer_first_name", DataLocal.getCustomer().getFirstName());
-			cutomer.put("customer_last_name", DataLocal.getCustomer().getLastName());
-			cutomer.put("customer_email", DataLocal.getCustomer().getEmail());
-			cutomer.put("customer_name", DataLocal.getCustomer().getName());
-			cutomer.put("customer_id", DataLocal.getCustomer().getID());
-		} catch (JSONException e) {
-			cutomer = null;
-		}
+        if (!Config.getInstance().getQuoteCustomerSignIn().equals("")) {
+            updateCustomerToQuoteModel.addDataExtendURL(Config.getInstance().getQuoteCustomerSignIn());
+        }
 
-		if (cutomer != null) {
-			updateCustomerToQuoteModel.addDataBody("customer", cutomer);
-		}
-		updateCustomerToQuoteModel.request();
-	}
+        if (!DataLocal.getQuoteCustomerNotSigin().equals("")) {
+            updateCustomerToQuoteModel.addDataExtendURL(DataLocal.getQuoteCustomerNotSigin());
+        }
+
+        JSONObject cutomer = null;
+        try {
+            cutomer = new JSONObject();
+            cutomer.put("customer_first_name", DataLocal.getCustomer().getFirstName());
+            cutomer.put("customer_last_name", DataLocal.getCustomer().getLastName());
+            cutomer.put("customer_email", DataLocal.getCustomer().getEmail());
+            cutomer.put("customer_name", DataLocal.getCustomer().getName());
+            cutomer.put("customer_id", DataLocal.getCustomer().getID());
+        } catch (JSONException e) {
+            cutomer = null;
+        }
+
+        if (cutomer != null) {
+            updateCustomerToQuoteModel.addDataBody("customer", cutomer);
+        }
+        updateCustomerToQuoteModel.request();
+    }
 }
