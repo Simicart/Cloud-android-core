9b2d171498583ec7a164cdec94e255ef01a85d26
dongln <dongln3011@gmail.com>
2016-02-24 10:18:08 +0700

    Fix bug cancel update payment in checkout.com

--- app/src/main/java/com/simicart/plugins/checkout/com/fragment/CheckoutComFragment.java
+++ app/src/main/java/com/simicart/plugins/checkout/com/fragment/CheckoutComFragment.java
@@ -5,14 +5,20 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.simicart.MainActivity;
+import com.simicart.core.base.delegate.ModelDelegate;
 import com.simicart.core.base.fragment.SimiFragment;
 import com.simicart.core.base.manager.SimiManager;
+import com.simicart.core.base.model.collection.SimiCollection;
+import com.simicart.core.base.network.request.error.SimiError;
 import com.simicart.core.checkout.entity.OrderEntity;
 import com.simicart.core.config.Config;
 import com.simicart.core.config.Rconfig;
@@ -20,6 +26,7 @@ import com.simicart.core.customer.controller.SignInController;
 import com.simicart.core.customer.entity.OrderHisDetail;
 import com.simicart.plugins.checkout.com.block.CheckoutComBlock;
 import com.simicart.plugins.checkout.com.controller.CheckoutComController;
+import com.simicart.plugins.checkout.com.model.CheckOutCancelModel;
 
 /**
  * Created by James Crabby on 1/15/2016.
@@ -105,12 +112,8 @@ public class CheckoutComFragment extends SimiFragment {
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog,
                                                 int which) {
-//                                requestCancelOrder();
-//                                if(!mPayment.getMessageCancel().equals("")){
-//                                    showToastMessage(mPayment.getMessageCancel());
-//                                }else{
-//                                    showToastMessage("Your order has been canceled!");
-//                                }
+                                requestCancelOrder();
+                                showToastMessage("Your order has been canceled");
                                 SimiManager.getIntance().backToHomeFragment();
                             }
                         })
@@ -122,6 +125,34 @@ public class CheckoutComFragment extends SimiFragment {
                             }
                         }).show();
 
+    }
+
+    private void requestCancelOrder(){
+        CheckOutCancelModel checkoutCancelModel = new CheckOutCancelModel();
+        checkoutCancelModel.setDelegate(new ModelDelegate() {
+            @Override
+            public void onFail(SimiError error) {
+                if(error != null){
+                    showToastMessage(error.getMessage());
+                }
+            }
+
+            @Override
+            public void onSuccess(SimiCollection collection) {
+
+            }
+        });
+        checkoutCancelModel.addDataExtendURL(orderID);
+        checkoutCancelModel.addDataExtendURL("cancel");
+        checkoutCancelModel.request();
+    }
+
+    public void showToastMessage(String message) {
+        Toast toast = Toast.makeText(MainActivity.context, Config.getInstance()
+                .getText(message), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.show();
     }
 
 }
